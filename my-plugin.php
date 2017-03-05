2fa704b667ce1bbe349ac8e840517f4871953afa
chriszarate <chris@zarate.org>
2017-03-05 12:31:10 -0500

    Plugin code and docker-compose setup.

---
+++ my-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     My Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     my-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Plugin
+ */
+
+/**
+ * My Plugin class
+ */
+class My_Plugin {}
