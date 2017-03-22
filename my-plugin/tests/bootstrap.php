c726af98873755391f8998e964741c7f39326b2c
chriszarate <chris@zarate.org>
2017-03-21 20:39:38 -0400

    working subdirectory structure

--- my-plugin/tests/bootstrap.php
+++ my-plugin/tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package My_Plugin
+ * @package My Plugin
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/class-my-plugin.php';
+	require dirname( dirname( __FILE__ ) ) . '/my-plugin.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
